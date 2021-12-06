d1e5ffbd799bf78987a3ad075d552a8512b03886
Dysrhythmic <jaketucker7@gmail.com>
2021-12-06 04:10:33 -0600

    add day 6 part 2 solution

--- day 6/lanternfish.js
+++ day 6/lanternfish.js
@@ -1,33 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 
-const maxFishTimer = 8;
-const days = 80;
+function getFishTimers(maxFishTimer) {
+    let fishTimers = [];
+    for (let i = 0; i <= maxFishTimer; i++) { fishTimers.push(BigInt(0)) }
+    fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split(',').forEach(timer => fishTimers[Number(timer)]++);
 
-let fishTimers = [];
-for (let i = 0; i <= maxFishTimer; i++) { fishTimers.push(0) }
-
-fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split(',').forEach(timer => fishTimers[Number(timer)]++);
-
-let newCount = 0;
-for (let i = 0; i < days; i++) {
-    newCount = 0;
-    for (timer in fishTimers) {
-        if (timer > 0) { 
-            fishTimers[timer - 1] += fishTimers[timer];
-            fishTimers[timer] = 0;
-            if (timer == 8) {
-                fishTimers[7] -= newCount;
-                fishTimers[timer] += newCount;
-            }
-        }
-        else { 
-            fishTimers[8] += fishTimers[timer];
-            newCount = fishTimers[timer];
-            fishTimers[timer] = 0;
-        }
-    }
-    fishTimers[6] += newCount;
+    return fishTimers;
 }
 
-console.log('sum:', fishTimers.reduce((acc, timerCount) => acc + timerCount))
+function countFish(days, fishTimers) {
+    for (let i = 0; i < days; i++) {
+        let newCount = BigInt(0);
+        for (timer in fishTimers) {
+            if (timer > 0) { 
+                fishTimers[timer - 1] += fishTimers[timer];
+                fishTimers[timer] = BigInt(0);
+                if (timer == 8) {
+                    fishTimers[7] -= newCount;
+                    fishTimers[timer] += newCount;
+                }
+            }
+            else { 
+                fishTimers[8] += fishTimers[timer];
+                newCount = fishTimers[timer];
+                fishTimers[timer] = BigInt(0);
+            }
+        }
+        fishTimers[6] += newCount;
+    }
+
+    return fishTimers.reduce((acc, timerCount) => acc + timerCount);
+}
+
+const maxFishTimer = 8;
+
+let fishTimers = getFishTimers(maxFishTimer);
+let totalFish = countFish(80, fishTimers);
+console.log('Part 1:', totalFish);
+
+fishTimers = getFishTimers(maxFishTimer);
+totalFish = countFish(256, fishTimers);
+console.log('Part 2:', totalFish);
