ad9a9924d1d187c3386943f496def8986c86bbc4
Dysrhythmic <jaketucker7@gmail.com>
2021-12-07 02:56:07 -0600

    remove redundant parenthesis

--- day 7/whaleTreachery.js
+++ day 7/whaleTreachery.js
@@ -5,7 +5,7 @@ const positions = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').spl
 positions.sort((a, b) => a - b);
 
 const medianPos = positions[Math.floor(positions.length / 2)];
-const totalFuelCost = positions.reduce((acc, pos) => acc + (Math.abs(medianPos - pos)), 0);
+const totalFuelCost = positions.reduce((acc, pos) => acc + Math.abs(medianPos - pos), 0);
 console.log('Part 1:', totalFuelCost)
 
 function getConsecutiveFuelCost(targetPos) {
